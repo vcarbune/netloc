193fbfdfd0a8f7db08e9cbb20b4bf64553772b82
Victor Carbune <vcarbune@ethz.ch>
2014-01-28 14:10:54 +0100

    there's no delta in avg plots

--- data/plots/plot_combined_avg.sh
+++ data/plots/plot_combined_avg.sh
@@ -5,7 +5,7 @@ for type in barabasi forest erdos; do
 		    file2='set6_eps_0.025/${type}_avg_test'; val2='0.025'; \
 		    file3='set7_eps_0.100/${type}_avg_test'; val3='0.100'; \
 		    outfile='${type}_eps_avgcombined_test'; \
-		    yl='avg delta of observers (%)'; var='Eps'; \
+		    yl='avg of observers (%)'; var='Eps'; \
 		    plottitle='Observers Needed (${type}, N=100)'" \
 	script_avgdiff_multi.gnu
 done
@@ -15,7 +15,7 @@ for type in barabasi forest erdos; do
 		    file2='set6_eps_0.025/${type}_avg_pb'; val2='0.025'; \
 		    file3='set7_eps_0.100//${type}_avg_pb'; val3='0.100'; \
 		    outfile='${type}_eps_avgcombined_pb'; \
-		    yl='avg delta of mass (%)'; var='Eps'; \
+		    yl='avg of mass (%)'; var='Eps'; \
 		    plottitle='Avg Mass Concentration (Solution Cluster) (${type}, N=100)'" \
 	script_avgdiff_multi.gnu
 done
@@ -25,7 +25,7 @@ for type in barabasi forest erdos; do
 		    file2='set8_gensteps_2/${type}_avg_test'; val2='2'; \
 		    file3='set9_gensteps_8/${type}_avg_test'; val3='8'; \
 		    outfile='${type}_steps_avgcombined_test'; \
-		    yl='avg delta of observers (%)'; var='Steps'; \
+		    yl='avg of observers (%)'; var='Steps'; \
 		    plottitle='Observers Needed (${type}, N=100)'" \
 	script_avgdiff_multi.gnu
 done
@@ -35,7 +35,7 @@ for type in barabasi forest erdos; do
 		    file2='set8_gensteps_2/${type}_avg_pb'; val2='2'; \
 		    file3='set9_gensteps_8/${type}_avg_pb'; val3='8'; \
 		    outfile='${type}_steps_avgcombined_pb'; \
-		    yl='avg delta of mass (%)'; var='Steps'; \
+		    yl='avg of mass (%)'; var='Steps'; \
 		    plottitle='Avg Mass Concentration (Solution Cluster) (${type}, N=100)'" \
 	script_avgdiff_multi.gnu
 done
